e5e9a899e8f78dd14e23b1bb4d745a2a19cde4e1
mohamezzz700 <mohamezzz700@gmail.com>
2025-05-23 21:00:39 +0000

    LoadingAuth with Bloc package

--- lib/feauters/auth/presentation/cubits/auth_cubit.dart
+++ lib/feauters/auth/presentation/cubits/auth_cubit.dart
@@ -12,8 +12,59 @@ class AuthCubit extends Cubit<AuthStates> {
   AuthCubit({required this.authRepo}) : super(AuthInitial());
 
   //check if user already authenticated
+  void checkAuth() async {
+    final AppUser? user = await authRepo.getCurrentUser();
+
+    if (user != null) {
+      _currentUser = user;
+      emit(Authenticated(user));
+    } else {
+      emit(Unauthenticated());
+    }
+  }
 
   //get current user
+  AppUser? get currentUser => _currentUser;
 
-  //login with email
+  //login with email+pw
+  Future<void> login(String email, String pw) async {
+    try {
+      emit(AuthLoading());
+      final user = await authRepo.loginWithEmailPassword(email, pw);
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(Unauthenticated());
+      }
+    } catch (e) {
+      emit(AuthErrors(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+  // register with email+pw
+  Future<void> register(String name, String email, String pw) async {
+    try {
+      emit(AuthLoading());
+      final user = await authRepo.registerWithEmailPassword(name,email, pw);
+
+      if (user != null) {
+        _currentUser = user;
+        emit(Authenticated(user));
+      } else {
+        emit(Unauthenticated());
+      }
+    } catch (e) {
+      emit(AuthErrors(e.toString()));
+      emit(Unauthenticated());
+    }
+  }
+
+  //logout
+  Future<void> logout() async {
+    authRepo.logout();
+    emit(Unauthenticated());
+  }
 }
